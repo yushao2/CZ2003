15feb6e4a5ff8c615713bced75b2dc5b4f650da1
Pang Yu Shao <p.yushao2@gmail.com>
2021-02-02 23:47:58 +0800

    Lab 1 completed

---
+++ labs/convertNM.py
@@ -0,0 +1,15 @@
+import os
+
+N_VALUE = "8"
+M_VALUE = "10"
+
+current_dir = os.getcwd()
+for i in [i for i in os.listdir() if os.path.isdir(i) and "venv" not in i]:
+    path = os.path.join(current_dir, i)
+    sources = [os.path.join(path,i) for i in os.listdir(path) if ".wrl" in i]
+    for filepath in sources: 
+        with open(filepath, "r") as f:
+            all_lines = f.read()
+        editted = all_lines.replace("<N>", N_VALUE).replace("<M>", M_VALUE)
+        with open(filepath, "w") as f:
+            f.write(editted)
